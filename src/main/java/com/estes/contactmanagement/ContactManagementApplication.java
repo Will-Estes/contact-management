4461c232111b22a1607986d200eb3a775fa6dad1
will-estes <will_estes@yahoo.com>
2025-01-22 07:15:24 -0700

    test

--- src/main/java/com/estes/contactmanagement/ContactManagementApplication.java
+++ src/main/java/com/estes/contactmanagement/ContactManagementApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContactManagementApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ContactManagementApplication.class, args);
+  }
 }
